928ea0363e425779eb5d7b6545262c6d8d0753c4
f2fx4553 <oussamafifi223@gmail.com>
2026-03-30 22:37:09 +0100

    bump: bump version to 1.1.0 to publish on PyPI

--- sibna/__init__.py
+++ sibna/__init__.py
@@ -22,7 +22,7 @@ Example usage:
     >>> decrypted = session.decrypt(encrypted)
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Sibna Security Team"
 __license__ = "Apache-2.0 OR MIT"
 
